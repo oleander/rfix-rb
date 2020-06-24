cdd7cb782642a50120a52e61a6b95058aa47086b
Linus Oleander <linus@oleander.nu>
2020-06-24 17:20:04 +0200

    ok

--- lib/rfix/rfix.rb
+++ lib/rfix/rfix.rb
@@ -202,7 +202,7 @@ module Rfix
 
   # Original branch, usually master
   def ref_since_origin
-    git("show-branch", "--merge-base").first
+    git("show-branch", "--fork-point").first
   end
 end
 
