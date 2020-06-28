2f7cd1e934eb327fd632d141cecc9343e260ae47
Linus Oleander <linus@oleander.nu>
2020-06-28 18:53:22 +0200

    Add a few more destroyed tests

--- spec/support/examples/a_destroyed_file.rb
+++ spec/support/examples/a_destroyed_file.rb
@@ -1,6 +1,6 @@
 RSpec.shared_examples "a destroyed file" do
   Change::FIXTURES.keys.each do |type|
-    fdescribe "a destroy file of type #{type}" do
+    describe "a destroy file of type #{type}" do
       describe "a destroyed file which has not been staged" do
         let(:file) { f(type).tracked.destroy }
         it { is_expected.to have_exit_status(0) }
@@ -17,6 +17,20 @@ RSpec.shared_examples "a destroyed file" do
 
       describe "a destroyed file which is tracked" do
         let(:file) { f(type).tracked.destroy.tracked }
+        it { is_expected.to have_exit_status(0) }
+        it { is_expected.to_not have_listed_file(file) }
+        it { is_expected.to_not have_linted_staged_file(file) }
+      end
+
+      describe "a file which was first staged then destroyed" do
+        let(:file) { f(type).staged.destroy }
+        it { is_expected.to have_exit_status(0) }
+        it { is_expected.to_not have_listed_file(file) }
+        it { is_expected.to_not have_linted_staged_file(file) }
+      end
+
+      describe "a file which was destroyed" do
+        let(:file) { f(type).destroy }
         it { is_expected.to have_exit_status(0) }
         it { is_expected.to_not have_listed_file(file) }
         it { is_expected.to_not have_linted_staged_file(file) }
