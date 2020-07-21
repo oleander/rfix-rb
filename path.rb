d566b2dab8b204ff633ee3306b5496bf4438e60c
Linus Oleander <linus@oleander.nu>
2020-07-21 10:10:42 +0200

    Clean up

--- path.rb
+++
@@ -1,7 +0,0 @@
-require "jsonpath"
-require "json"
-
-json = JSON.parse(File.read("spec/fixtures/rubocop.json"))
-result = JsonPath.new("files[:][?(@.offenses[:].corrected==\"true\")].path").on(json)
-
-puts result
