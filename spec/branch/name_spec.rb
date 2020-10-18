ae227812091230b8748ce54026939d454fc96542
Linus Oleander <linus@oleander.nu>
2020-10-18 11:31:31 +0200

    Fix failing test

--- spec/branch/name_spec.rb
+++ spec/branch/name_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Rfix::Branch::Name do
   describe "#resolve(with:)", :git do
     describe "a named branch" do
-      let(:branch) { Rfix::Branch::Name.new("master").branch(using: repo) }
+      let(:branch) { Rfix::Branch::Name.new("master").resolve(with: repo) }
 
       it "resolves to named branch with commits", checkout: "stable", commits: 3 do
         expect(branch.name).to eq("master")
--- spec/branch/upstream_spec.rb
+++ spec/branch/upstream_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Rfix::Branch::Upstream do
   describe "#resolve(with:)", :git do
     describe "a named branch with upstream", checkout: "master", upstream: "master" do
-      let(:branches) { Rfix::Branch::UPSTREAM.names(using: repo).map(&:name) }
+      let(:branches) { Rfix::Branch::UPSTREAM.resolve(with: repo).map(&:name) }
 
       context "no commits" do
         context "same branch as upstream", checkout: "master" do
