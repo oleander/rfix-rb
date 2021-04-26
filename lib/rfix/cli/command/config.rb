4d7c97a8e86325b75c8e34657772e44622af7984
John Doe <not-my@real-email.com>
2021-04-26 03:19:56 +0200

    rrrrr

---
+++ lib/rfix/cli/command/config.rb
@@ -0,0 +1,22 @@
+require "rubocop"
+
+module Rfix
+  module CLI
+    module Command
+      class Config < RuboCop::CommentConfig
+        include Log
+
+        def initialize(rfix, *rest)
+          super(*rest)
+          @rfix = rfix
+        end
+
+        def cop_enabled_at_line?(_, line)
+          super && @rfix.include?(processed_source.file_path, line)
+        rescue StandardError => e
+          say! e.message
+        end
+      end
+    end
+  end
+end
--- lib/rfix/cli/command/lint.rb
+++ lib/rfix/cli/command/lint.rb
@@ -12,18 +12,6 @@ module Rfix
         option :auto_correct, type: :boolean, default: true
         option :branch, type: :string
 
-        Config = Class.new(RuboCop::CommentConfig) do
-          def initialize(rfix, *rest)
-            super(*rest)
-            @rfix = rfix
-          end
-
-          def cop_enabled_at_line?(_, line)
-            super && @rfix.include?(processed_source.file_path, line)
-          rescue StandardError => e
-            puts e.message
-          end
-        end
 
         def call(args: [], branch: "master", **params)
           options  = RuboCop::Options.new
