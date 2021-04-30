2aaff7102acda029e3cf93fc7e6471ff1144f17a
John Doe <not-my@real-email.com>
2021-04-30 19:38:50 +0200

    Remove old code

--- lib/rfix/commands/help.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-extend Rfix::Log
-
-summary "Displays help"
-
-run do |_opts, _args, cmd|
-  prt cmd.supercommand.help
-end
--- lib/rfix/commands/helper/help.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-extend Rfix::Log
-
-flag   :h,  :help, "show help for this command" do |_value, cmd|
-  prt cmd.help
-  exit 0
-end
