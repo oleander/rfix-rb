50cdd661d68ec723942d19dd22470994304410b1
John Doe <not-my@real-email.com>
2021-05-20 13:33:58 +0200

    autoload

--- lib/rfix.rb
+++ lib/rfix.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
-require "active_support/core_ext/module/concerning"
 require "active_support/core_ext/module/attribute_accessors"
+require "active_support/core_ext/module/concerning"
 require "zeitwerk"
 require "rubocop"
-require "pastel"
 
-require "rfix/extension/strings"
-require "rfix/extension/pastel"
+Dir["rfix/extension/**/*"].sort.each(&method(:require))
 
 loader = Zeitwerk::Loader.for_gem
 loader.ignore("#{__dir__}/rfix/rake/paths")
 loader.ignore("#{__dir__}/rfix/rake/support")
 loader.ignore("#{__dir__}/rfix/loader")
 loader.ignore("#{__dir__}/rfix/rake")
-loader.ignore("#{__dir__}/rfix/extension/strings")
-loader.ignore("#{__dir__}/rfix/extension/pastel")
+loader.ignore("#{__dir__}/rfix/extension")
 loader.ignore("#{__dir__}/rfix/commands")
 loader.inflector.inflect "cli" => "CLI"
 loader.setup
@@ -24,5 +21,5 @@ module Rfix
   mattr_accessor :repo, :test
 end
 
-RuboCop::CommentConfig.prepend(Rfix::Extension::CommentConfig)
+require "rfix/extension/offense"
 RuboCop::Cop::Offense.prepend(Rfix::Extension::Offense)
--- lib/rfix/extension/comment_config.rb
+++ lib/rfix/extension/comment_config.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-module Rfix
-  module Extension
-    module CommentConfig
+require "rubocop"
+
+module RuboCop
+  class CommentConfig
+    concerning :Fallback, prepend: true do
       def cop_enabled_at_line?(_, line)
         super && repository.include?(processed_source.file_path, line)
       rescue StandardError => e
