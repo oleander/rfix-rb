2cd08a7c4e25d65462c1889e300f37a50eb3cdf0
John Doe <not-my@real-email.com>
2021-05-23 19:26:02 +0200

    Fix info spec

--- spec/integration/cli/command/info_spec.rb
+++ spec/integration/cli/command/info_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-RSpec.xdescribe Rfix::CLI::Command::Info, repository: "HEAD" do
+RSpec.describe Rfix::CLI::Command::Info, repository: "HEAD" do
   it_behaves_like "a command", "info"
 end
