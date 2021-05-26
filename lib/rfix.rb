d5020cc5b003d70bd15257753911c3d44ea49f87
Linus Oleander <linus@oleander.nu>
2021-05-27 01:30:33 +0200

    Dummy commit

--- lib/rfix.rb
+++ lib/rfix.rb
@@ -14,6 +14,7 @@ loader.ignore("#{__dir__}/rfix/rake")
 
 loader.inflector.inflect "cli" => "CLI"
 
+# Lazy ...
 loader.on_load("Rfix::Formatter") do
   Pathname(__dir__).glob("rfix/extension/*", &method(:require))
 end
