e3dd93176330bd5e03d1ce093129ba33668780f7
Linus Oleander <linus@oleander.nu>
2021-05-27 00:10:12 +0200

    Proper path matcher

--- lib/rfix/diff.rb
+++ lib/rfix/diff.rb
@@ -8,6 +8,7 @@ module Rfix
     # https://github.com/libgit2/rugged/blob/master/ext/rugged/rugged_diff.c
 
     attribute :options, Types::Hash.default(EMPTY_HASH)
+    attribute :current_path, Types::Path::Relative.default(Pathname(".").freeze)
     attribute :repository, Repository
 
     delegate :index, :origin, to: :repository
@@ -20,7 +21,7 @@ module Rfix
       ignore_whitespace: true,
       ignore_whitespace_change: true,
       ignore_whitespace_eol: true,
-      disable_pathspec_match: true,
+      disable_pathspec_match: false,
       ignore_submodules: true,
       include_ignored: false,
       include_unmodified: false,
@@ -34,6 +35,12 @@ module Rfix
       diff.deltas.reject(&:deleted?)
     end
 
+    def files
+      deltas.map(&:new_file).map do |file|
+        repository.path.join(file.fetch(:path))
+      end
+    end
+
     private
 
     def diff
@@ -43,8 +50,20 @@ module Rfix
       end
     end
 
+    def absolute_path
+      repository.path.join(current_path)
+    end
+
     def extended_options
-      OPTIONS.merge(**options)
+      unless absolute_path.exist?
+        raise Error, "#{current_path} path does not exist in #{repository.path}"
+      end
+
+      unless absolute_path.directory?
+        raise Error, "#{current_path} is not a directory"
+      end
+
+      OPTIONS.merge(**options, paths: [current_path.join("**/*").to_s])
     end
   end
 end
---
+++ spec/unit/diff_spec.rb
@@ -0,0 +1,55 @@
+
+ RSpec.shared_examples_for "a diff" do
+  its("lines.to_a") { is_expected.not_to be_empty }
+  its("files.to_a") { is_expected.not_to be_empty }
+  its("deltas.to_a") { is_expected.not_to be_empty }
+ end
+
+RSpec.describe Rfix::Diff, repository: "HEAD~50" do
+  context "when in non-empty sub folder" do
+    it_behaves_like "a diff" do
+      subject { described_class.new(current_path: "lib", repository: repository) }
+
+      its("files.to_a") { is_expected.to all(exist) }
+    end
+  end
+
+  context "when in an empty sub folder" do
+    its("lines.to_a") { is_expected.to be_empty }
+    its("files.to_a") { is_expected.to be_empty }
+    its("deltas.to_a") { is_expected.to be_empty }
+
+    let(:empty_path) { repository.path.join("empty") }
+
+    before do
+      empty_path.mkdir
+    end
+
+    subject { described_class.new(current_path: "empty", repository: repository) }
+  end
+
+  context "when in an non-existing sub folder" do
+    subject { described_class.new(current_path: "does-not-exist", repository: repository) }
+
+    its("lines.to_a") { will raise_error(Rfix::Error) }
+    its("files.to_a") { will raise_error(Rfix::Error) }
+    its("deltas.to_a") { will raise_error(Rfix::Error) }
+  end
+
+  context "when given a file instead of a directory" do
+    let(:current_file) { Pathname(repository.paths.first).relative_path_from(repository.path) }
+    subject { described_class.new(current_path: current_file, repository: repository) }
+
+    its("lines.to_a") { will raise_error(Rfix::Error) }
+    its("files.to_a") { will raise_error(Rfix::Error) }
+    its("deltas.to_a") { will raise_error(Rfix::Error) }
+  end
+
+  context "when in project folder" do
+    it_behaves_like "a diff" do
+      subject { described_class.new(current_path: ".", repository: repository) }
+
+      its("files.to_a") { is_expected.to all(exist) }
+    end
+  end
+end
