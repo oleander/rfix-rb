2c9d7cad85fe8e60c7faae9b5e27b729f38b1e15
Linus Oleander <linus@oleander.nu>
2021-05-27 01:59:39 +0200

    Simpler specs

--- spec/unit/branch/reference_spec.rb
+++ spec/unit/branch/reference_spec.rb
@@ -1,21 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Rfix::Branch::Reference do
-  context "when reference exists", repository: "HEAD" do
-    subject { branch }
+RSpec.describe Rfix::Branch::Reference, repository: "HEAD~10" do
+  subject { branch }
 
-    its(:resolve) { is_expected.to be_a(Rugged::Commit) }
-  end
-
-  context "when reference exists", repository: "HEAD~2" do
-    subject { branch }
-
-    its(:resolve) { is_expected.to be_a(Rugged::Commit) }
-  end
-
-  context "when reference is a branch name", repository: "master" do
-    subject { branch }
-
-    its(:resolve) { is_expected.to be_a(Rugged::Commit) }
-  end
+  its(:resolve) { is_expected.to be_a(Rugged::Commit) }
 end
